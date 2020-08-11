204aaff71e140d72dd7baa47b825a8aea3af55b0
Justin Chow <jchowchow@gmail.com>
2020-08-11 16:41:02 -0400

    Comment and format python script

--- script/redis_insert.py
+++ script/redis_insert.py
@@ -1,3 +1,6 @@
+# This script generates fruit stock & prices,
+# and randomly increments/decrements their numbers.
+
 import time
 import random
 import redis
@@ -5,6 +8,9 @@ import redis
 r = redis.Redis(host="localhost", port=6379, db=0)
 fruits = ("apple", "banana", "cherry")
 
+
+# Creates stock and price keys for every fruit in fruits.
+# Initializes values to 50.
 def create_keys():
     for fruit in fruits:
         r.execute_command('TS.CREATE stock:{0} LABELS label stock fruit {0}'.format(fruit))
@@ -13,12 +19,15 @@ def create_keys():
         r.execute_command('TS.ADD stock:{0} * 50'.format(fruit))
         r.execute_command('TS.ADD price:{0} * 50'.format(fruit))
 
+
+# Randomly increments/decrements a random fruit for the given label by 1-5 units.
 def update_random(label):
     fruit = fruits[random.randint(0, 2)]
     amount = random.randint(1, 5)
     command = "TS.INCRBY" if bool(random.getrandbits(1)) else "TS.DECRBY"
 
     r.execute_command("{0} {1}:{2} {3}".format(command, label, fruit, amount))
+
 
 if __name__ == "__main__":
     create_keys()
